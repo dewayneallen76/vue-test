316bd6a8cf09833243ea9a0acc9e3004dfbec092
Dewayne Allen <dewayneallen76@gmail.com>
2017-01-20 13:28:55 -0600

    REMOVED LINE FROM TASKS

--- public/js/lists.js
+++ public/js/lists.js
@@ -32,8 +32,7 @@ new Vue({
 			{ body: 'Go to the store', completed: false },
 			{ body: 'Go to the bank', completed: false },
 			{ body: 'Go to the doctor', completed: false }, 
-			{ body: 'Clean the house', completed: false },
-			{ body: 'Take parentls to the airport', completed: false }
+			{ body: 'Clean the house', completed: false }
 		]
 	}
 });
