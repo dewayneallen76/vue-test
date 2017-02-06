2fd044f48d61ac5d27a4627181bcba31662c09a5
Dewayne Allen <dewayneallen76@gmail.com>
2017-02-06 13:23:47 -0600

    added computed properties for the different error messages

--- public/js/alert.js
+++ public/js/alert.js
@@ -3,10 +3,23 @@ Vue.component('alert', {
 
 	template: '#alert-template',
 
+	props: ['type'],
+
 	data: function() {
 		return {
 			show: true
 		};
+	}, 
+
+	computed: {
+		alertClasses: function() {
+			var type = this.type;
+			return {
+				'Alert' : true,
+				'Alert--Success' : type == 'success',
+				'Alert--Error' : type == 'error'
+			}
+		}
 	}
 });
 
